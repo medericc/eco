76c40230c9f70e12d1387140a8bbbce379810a54
medericc <bleublanctour1@>
2025-03-26 20:06:49 +0100

    ee

--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -8,7 +8,7 @@ const Generating = ({ className }) => {
       } text-base`}
     >
       <img className="w-5 h-5 mr-4" src={loading} alt="Loading" />
-      AI is generating
+       <span className="text-sm">Dieu a tant aimé le monde</span>
     </div>
   );
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -78,11 +78,43 @@ export const notificationImages = [notification4, notification3, notification2];
 export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
 
 export const brainwaveServices = [
-  "Photo generating",
-  "Photo enhance",
-  "Seamless Integration",
+  "La déforestation",
+  "La pollution",
+  "Le changement climatique",
+  "La surexploitation",
+  "La perte de biodiversité",
 ];
 
+export const brainwaveServices2 = [
+  "Transports",
+  "Industries",
+  "Bâtiments",
+  "Agriculture et élevage",
+  "Production d’énergie",
+];
+
+export const brainwaveServices3 = [
+  "Transition énergétique",
+  "Transports durables",
+  "Agroforesterie",
+  "Efficacité énergétique",
+  "Techniques de capture du CO₂",
+];
+
+export const brainwaveServices4 = [
+  "Montée des eaux à Kiribati",
+  "Acidification des océans",
+  "Pollution et santé",
+  "Perte de biodiversité",
+  "Incendies de forêts",
+];
+export const brainwaveServices5 = [
+  "La forêt est 9 % de la Terre",
+  "La forêt est un outil de stockage du CO₂",
+  "1 personne sur 8 a directement besoin des forêts",
+  "L'agriculture est sa cause principale",
+  "En 30 ans on a perdu une quantité équivalente à la Libye",
+];
 export const brainwaveServicesIcons = [
   recording03,
   recording01,
@@ -244,16 +276,16 @@ export const pricing = [
 export const benefits = [
   {
     id: "0",
-    title: "Ask anything",
-    text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
+    title: "L'Écologie",
+    text: "Étude des interactions entre les organismes vivants et leur environnement, avec le milieu physique ou avec d'autres êtres vivants.",
     backgroundUrl: "./src/assets/benefits/card-1.svg",
     iconUrl: benefitIcon1,
     imageUrl: benefitImage2,
   },
   {
     id: "1",
-    title: "Improve everyday",
-    text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
+    title: "L'Écosystème",
+    text: "Ensemble d’interactions entre les organismes et leur environnement physique, ils forment la biosphère, l’ensemble des zones où la vie est possible sur Terre.",
     backgroundUrl: "./src/assets/benefits/card-2.svg",
     iconUrl: benefitIcon2,
     imageUrl: benefitImage2,
@@ -261,16 +293,16 @@ export const benefits = [
   },
   {
     id: "2",
-    title: "Connect everywhere",
-    text: "Connect with the AI chatbot from anywhere, on any device, making it more accessible and convenient.",
+    title: "Les Biomes",
+    text: "Grandes zones climatiques regroupant des écosystèmes similaires, qui sont influencés par des facteurs abiotiques et biotiques.",
     backgroundUrl: "./src/assets/benefits/card-3.svg",
     iconUrl: benefitIcon3,
     imageUrl: benefitImage2,
   },
   {
     id: "3",
-    title: "Fast responding",
-    text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
+    title: "Interactions Biologiques",
+    text: "Dans un écosystème, un organisme peut en manger un autre, deux espèces peuvent tirer profit de leur relation ou lutter pour une ressource limitée, vivre au dépens d'un autre ou encore profiter d'une autre espèce sans lui nuire.",
     backgroundUrl: "./src/assets/benefits/card-4.svg",
     iconUrl: benefitIcon4,
     imageUrl: benefitImage2,
@@ -278,16 +310,32 @@ export const benefits = [
   },
   {
     id: "4",
-    title: "Ask anything",
-    text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
+    title: "La Biodiversité",
+    text: "Essentielle à la stabilité des écosystèmes, plus un écosystème est diversifié, plus il résiste aux perturbations grâce aux interactions biologiques.",
     backgroundUrl: "./src/assets/benefits/card-5.svg",
     iconUrl: benefitIcon1,
     imageUrl: benefitImage2,
   },
   {
     id: "5",
-    title: "Improve everyday",
-    text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
+    title: "Les Corridors Écologiques",
+    text: "Passages permettant aux espèces de se déplacer entre des habitats fragmentés qui empêche les espèces de migrer et réduit la diversité génétique et augmente le risque d’extinction.",
+    backgroundUrl: "./src/assets/benefits/card-6.svg",
+    iconUrl: benefitIcon2,
+    imageUrl: benefitImage2,
+  },
+  {
+    id: "6",
+    title: "Le Forçage Radiatif",
+    text: "La différence entre l’énergie solaire absorbée par la Terre et celle renvoyée dans l’espace. Si ce forçage est positif, la Terre se réchauffe car il y a plus d’énergie retenue.",
+    backgroundUrl: "./src/assets/benefits/card-6.svg",
+    iconUrl: benefitIcon2,
+    imageUrl: benefitImage2,
+  },
+  {
+    id: "7",
+    title: "Le Gulf Stream",
+    text: "Courant chaud transportant la chaleur vers l’Europe, il ralentit à cause du réchauffement et donc peut provoquer des hivers plus froids en Europe et des étés plus chauds.",
     backgroundUrl: "./src/assets/benefits/card-6.svg",
     iconUrl: benefitIcon2,
     imageUrl: benefitImage2,
