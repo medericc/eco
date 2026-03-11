bafa46043b63d3e5afd3089f8c708e9a606b3541
medericc <bleublanctour1@>
2026-03-11 05:21:26 +0100

    ee

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -6,9 +6,9 @@ import { GradientLight } from "./design/Base";
 import ClipPath from "../assets/svg/ClipPath";
 
 const links: { [key: string]: string } = {
-  "0": "https://youtu.be/x2_WUWkrG7A?si=Hdq7djQItUb0o_rd",
   "1": "https://youtu.be/x2_WUWkrG7A?si=Hdq7djQItUb0o_rd",
   "2": "https://youtu.be/x2_WUWkrG7A?si=Hdq7djQItUb0o_rd",
+  "3": "https://youtu.be/x2_WUWkrG7A?si=Hdq7djQItUb0o_rd",
   // "3": "https://www.youtube.com/watch?v=kaeObGnMvEc",
   // "4": "https://www.youtube.com/watch?v=UHvqF-hT03o",
   // "5": "https://www.youtube.com/watch?v=808k5GVQMvE",
